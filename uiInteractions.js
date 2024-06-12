19b5a4bb4044bbed18597c505e7bfc682ffbb501
Martin Bozhilov <bozhilov53@gmail.com>
2024-06-12 19:14:38 +0300

    enable menu interaction through keys

--- uiInteractions.js
+++ uiInteractions.js
@@ -13,12 +13,29 @@ const getLongestLiElementWidth = (elementsArr) => {
 }
 
 const switchCurrentBtn = (event) => {
-  const clickedBtn = event.currentTarget;
+  const clickedBtn = event.type === 'click' ? event.currentTarget : event.target;
   const siblingBtns = Array.from(clickedBtn.parentElement.children);
   siblingBtns.forEach((button) => {
     button.classList.remove('current');
-  })
+  })    
   clickedBtn.classList.add('current');
+}
+
+const expandSideNav = () => {
+  const menuLinks = Array.from(sideNavIconsCont.querySelectorAll('.side-nav-link > span'))
+  const longestElementWidth = getLongestLiElementWidth(menuLinks)
+
+  sideNav.style.paddingRight = `calc(5rem + ${Math.ceil(longestElementWidth / 2)}px)`;
+  sideNav.classList.add('expanded-menu')
+  itemDesc.classList.add('expanded-menu')
+  itemsGrid.classList.add('expanded-menu')
+}
+
+const collapseSideNav = () => {
+  sideNav.style.paddingRight = '5rem';
+  itemDesc.classList.remove('expanded-menu')
+  itemsGrid.classList.remove('expanded-menu')
+  sideNav.classList.remove('expanded-menu')
 }
 
 Array.from(sideNavIconsCont.children).forEach((iconBtn) => {
@@ -29,19 +46,15 @@ Array.from(headerNav.children).forEach((iconBtn) => {
   iconBtn.addEventListener('click', switchCurrentBtn)
 })
 
-sideNavIconsCont.addEventListener('mouseenter', () => {
-  const menuLinks = Array.from(sideNavIconsCont.querySelectorAll('.side-nav-link > span'))
-  const longestElementWidth = getLongestLiElementWidth(menuLinks)
+sideNavIconsCont.addEventListener('mouseenter', expandSideNav)
+sideNavIconsCont.addEventListener('focusin', expandSideNav)
+sideNav.addEventListener('mouseleave', collapseSideNav)
+sideNav.addEventListener('focusout', collapseSideNav)   
 
-  sideNav.style.paddingRight = `calc(5rem + ${Math.ceil(longestElementWidth / 2)}px)`;
-  sideNav.classList.add('expanded-menu')
-  itemDesc.classList.add('expanded-menu')
-  itemsGrid.classList.add('expanded-menu')
-})
-
-sideNav.addEventListener('mouseleave', () => {
-  sideNav.style.paddingRight = '5rem';
-  itemDesc.classList.remove('expanded-menu')
-  itemsGrid.classList.remove('expanded-menu')
-  sideNav.classList.remove('expanded-menu')
+interactionManager.keyboard.on({
+  keys: ['Enter'],
+  callback: (event) => {
+    switchCurrentBtn(event) 
+  },
+  type: ['press'],
 })
