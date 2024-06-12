0ad20ae953c3ac3637d693502d1a1c58308e346e
Martin Bozhilov <bozhilov53@gmail.com>
2024-06-12 15:15:02 +0300

    Improve side nav interaction animation

--- index.js
+++ index.js
@@ -19,14 +19,28 @@ const sideNavIconsCont = document.querySelector('.side-nav-links')
 const itemDesc = document.querySelector('.items-desc')
 const itemsGrid = document.querySelector('.items-grid')
 
-console.log(sideNav)
+const getLongestLiElementWidth = (elementsArr) => {
+  const longestLink = elementsArr.reduce((acc, current) => {
+    return acc.offsetWidth > current.offsetWidth ? acc : current
+  }, elementsArr[0].offsetWidth)
+
+  return longestLink.offsetWidth;
+}
+
 sideNavIconsCont.addEventListener('mouseenter', () => {
+  const menuLinks = Array.from(sideNavIconsCont.querySelectorAll('.side-nav-link > span'))
+  const longestElementWidth = getLongestLiElementWidth(menuLinks)
+
+  sideNav.style.paddingRight = `calc(5rem + ${Math.ceil(longestElementWidth / 2)}px)`;
   sideNav.classList.add('expanded-menu')
   itemDesc.classList.add('expanded-menu')
   itemsGrid.classList.add('expanded-menu')
 })
+
 sideNav.addEventListener('mouseleave', () => {
+  sideNav.style.paddingRight = '5rem';
   itemDesc.classList.remove('expanded-menu')
   itemsGrid.classList.remove('expanded-menu')
   sideNav.classList.remove('expanded-menu')
 })
+
