0b8d557e72825bcecf831e0e7d444c291fbd7b58
Martin Bozhilov <bozhilov53@gmail.com>
2024-06-12 10:49:10 +0300

    Format code using ESLint and Prettier

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const rootElement = document.getElementsByTagName('html')[0];
 
 function resize() {
-    const windowWidth = window.innerWidth;
-    const scaleFactorX = windowWidth / 800;
+  const windowWidth = window.innerWidth;
+  const scaleFactorX = windowWidth / 800;
 
-    const fontSize = 10 * scaleFactorX;
+  const fontSize = 10 * scaleFactorX;
 
-    rootElement.style.fontSize = `${fontSize}px`;
+  rootElement.style.fontSize = `${fontSize}px`;
 }
 
 resize();
